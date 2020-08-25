0cd221cf6efac64f7edcdad82be3c100ed5dafc5
Sandeep Sihari <kumarsandeep91@gmail.com>
2020-08-26 00:02:28 +0530

    infinite scroll done

--- src/components.tsx
+++ src/components.tsx
@@ -1,15 +1,18 @@
 import * as React from "react";
 import { useStyletron } from "styletron-react";
-import InfiniteScroll from "react-infinite-scroll-component";
+import InfiniteScroll from "react-infinite-scroller";
 
 const Questions = () => {
   const [css] = useStyletron();
   const [hasMore, setHasMore] = React.useState(false);
   const [items, setItems] = React.useState([]);
   const [page, setPage] = React.useState(1);
+  const [loading, setLoading] = React.useState(false);
 
   React.useEffect(() => {
-    const url = `https://api.stackexchange.com/2.2/questions?page=${page}&order=desc&sort=activity&site=stackoverflow`;
+    setLoading(true);
+    // const url = `https://api.stackexchange.com/2.2/questions?page=${page}&order=desc&sort=activity&site=stackoverflow`;
+    const url = "";
     fetch(url)
       .then((res) => res.json())
       .then((data) => {
@@ -23,53 +26,47 @@ const Questions = () => {
           );
         });
 
-        setItems([...items].concat(rows));
-        setHasMore(data.hasMore);
+        setItems(items.concat(rows));
+        setHasMore(data.has_more);
+        setLoading(false);
       });
   }, [page]);
 
-  const handleNext = () => {
-    console.log("????? Fetching Next ?????????????");
-    setPage(page + 1);
+  const loadMore = async () => {
+    if (loading) return;
+    await setPage(page + 1);
   };
 
+  console.log("Array length ????");
+  console.log(items.length);
   return (
-    <div className={css({ display: "grid", gridGap: "8px" })}>
-      <div
-        className={css({
-          display: "grid",
-          gridGap: "8px",
-          gridTemplateColumns: "auto auto auto",
-        })}
-      >
-        <h6>Author</h6>
-        <h6>Title</h6>
-        <h6>Creation Date</h6>
-      </div>
-      {/* Data from stackoverflow */}
-      <InfiniteScroll
-        dataLength={items.length} //This is important field to render the next data
-        next={handleNext}
-        hasMore={hasMore}
-        loader={<h4>Loading...</h4>}
-        endMessage={
-          <p style={{ textAlign: "center" }}>
-            <b>Yay! You have seen it all</b>
-          </p>
-        }
-        // below props only if you need pull down functionality
-        // refreshFunction={this.refresh}
-        // pullDownToRefresh
-        // pullDownToRefreshContent={
-        //   <h3 style={{ textAlign: "center" }}>&#8595; Pull down to refresh</h3>
-        // }
-        // releaseToRefreshContent={
-        //   <h3 style={{ textAlign: "center" }}>&#8593; Release to refresh</h3>
-        // }
-      >
-        {items}
-      </InfiniteScroll>
-    </div>
+    // <div className={css({ display: "grid", gridGap: "8px" })}>
+    //   <div
+    //     className={css({
+    //       display: "grid",
+    //       gridGap: "8px",
+    //       gridTemplateColumns: "auto auto auto",
+    //     })}
+    //   >
+    //     <h6>Author</h6>
+    //     <h6>Title</h6>
+    //     <h6>Creation Date</h6>
+    //   </div>
+    //   {/* Data from stackoverflow */}
+
+    // </div>
+    <InfiniteScroll
+      pageStart={0}
+      loadMore={loadMore}
+      hasMore={hasMore}
+      loader={
+        <div className="loader" key={0}>
+          Loading ...
+        </div>
+      }
+    >
+      {items}
+    </InfiniteScroll>
   );
 };
 
