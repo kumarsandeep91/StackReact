3cf6a10375d20237cfad3dbec55ec3f08d2b040c
Sandeep Sihari <kumarsandeep91@gmail.com>
2020-08-26 00:17:14 +0530

    implementing stackoverflow official theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,35 @@
 import React from "react";
 import Questions from "./components";
+import { useStyletron } from "styletron-react";
 
 const App = () => {
+  const [css] = useStyletron();
+
   return (
     <div>
-      <header>
-        <h1>StackReact</h1>
+      <header
+        className={css({
+          backgroundColor: "#fafafb",
+          padding: "1rem",
+          display: "grid",
+          gridTemplateColumns: "auto auto",
+          gridGap: "0.5rem",
+          position: "sticky",
+          borderTop: "3px solid #f48024",
+        })}
+      >
+        <h1
+          className={css({
+            margin: "0px",
+            justifySelf: "end",
+            color: "#f48024",
+          })}
+        >
+          React
+        </h1>
+        <h1 className={css({ margin: "0px", justifySelf: "start" })}>
+          Overflow
+        </h1>
       </header>
 
       <main>
